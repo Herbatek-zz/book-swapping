2fddfeae208ebbab46fe8241dddf2e9984c24cc3
kenshin <kenshin985@gmail.com>
2018-03-05 21:42:52 +0100

    init commit

---
+++ src/main/java/com/piotrek/bookswapping/BookSwappingApplication.java
@@ -0,0 +1,12 @@
+package com.piotrek.bookswapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSwappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSwappingApplication.class, args);
+	}
+}
